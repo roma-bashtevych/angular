00a7f43e76b22399ce38e73b71c556f1ce2fa9c9
roma-bashtevych <roma_bashtevich@ukr.net>
2021-07-25 11:35:09 +0300

    show users homework1

---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {UserService} from "../../services/user.service";
+import {IUser} from "../../models/IUser";
+
+@Component({
+  selector: 'app-users',
+  templateUrl: './users.component.html',
+  styleUrls: ['./users.component.scss']
+})
+export class UsersComponent implements OnInit {
+
+  users: IUser[];
+
+  constructor(private userService: UserService) {
+    this.userService.getUsers().subscribe(value => {
+      this.users = value;
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {IUser} from "../models/IUser";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+
+
+  getUsers() :Observable<IUser[]> {
+   return this.httpClient.get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+  }
+}
